f4cb696ff8d1a0635c786ee3a37b19a8f0b2a0fa
unknown <you@example.com>
2023-12-03 20:54:27 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,26 @@
+
+	import { YStack } from "tamagui";
+	import { Container, Main, Title, Subtitle, Button, ButtonText } from '../tamagui.config';
+
+
+import { Link } from "expo-router";
+
+export default function Page() {
+	return (
+		
+			<Container>
+				<Main>
+				<YStack>
+					<Title>Hello World</Title>
+					<Subtitle>This is the first page of your app.</Subtitle>
+				</YStack>
+				<Link href={{ pathname: "/details", params: { name: "Dan" } }} asChild>
+					<Button><ButtonText>Show Details</ButtonText></Button>
+				</Link>
+				</Main>
+			</Container>
+			
+	);
+}
+
+
